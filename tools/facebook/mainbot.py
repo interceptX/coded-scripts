f8ffdc837d33d0dc0ccf0cdd96d85aa63318df43
interceptX <gulanemarvel@gmail.com>
2024-06-04 08:58:16 +0800

    directory created and modify script

--- tools/facebook/mainbot.py
+++ tools/facebook/mainbot.py
@@ -9,43 +9,65 @@ class EchoBot(Client):
         self.markAsDelivered(thread_id, message_object.uid)
         self.markAsRead(thread_id)
 
+        def sendmsg():
+            if (author_id != self.uid):
+                self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
+
+
         def welcome_command():
-            response = "Hey! I am MarvelBot Version 1.0\n\nFeel free to use 'help' command to know more."
-            self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+            txt  = "/home/interceptX/notes/tools/facebook/data/banner.txt"
+            with open(txt,'r') as file:
+                lines = file.read()
+            reply = lines
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
 
         def help_command(): 
-            response = "\nMarvelBot v1.0 commands:\n\n- info  = bot information\n- zoom  = zoom link meeting\n- report = banning facebook target"
-            self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+            txt  = "/home/interceptX/notes/tools/facebook/data/help.txt"
+            with open(txt,'r') as file:
+                lines = file.read()
+            reply = lines
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
+
+        def adel_info(): 
+            txt  = "/home/interceptX/notes/tools/facebook/data/adel.txt"
+            with open(txt,'r') as file:
+                lines = file.read()
+            reply = lines
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
+
 
         def info_command():
-            response = "\nProg Language: Python 3.11\nBot name: MarvelBot\nVersion: 1.0\nLocation: Philippines\nStatus: Retard\nDeveloper: Marvel Gulane"
-            self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+            txt = "/home/interceptX/notes/tools/facebook/data/info.txt"
+            with open(txt, 'r') as file:
+                lines = file.read()
+            reply = lines
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
 
         def zoom_command():
-            response = "\nZoom account not yet setup!"
-            self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+            reply = "\nzoom account not yet setup!"
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
 
         def report_command():
-            response = "\nCommand: report <target_user_id>\n\nThis might result in banning a facebook user!"
-            self.send(Message(text=response), thread_id=thread_id, thread_type=thread_type)
+            reply = "\nCommand: report {target_id}\nNote: This might result banning facebook user."
+            self.send(Message(text=reply), thread_id=thread_id, thread_type=thread_type)
 
         def troll_command():
-            txt  = "/home/interceptX/notes/tools/facebook/troll.txt"
+            txt  = "/home/interceptX/notes/tools/facebook/data/troll.txt"
             with open(txt,'r') as file:
                 lines = file.readlines()
             random_line = random.choice(lines)
             self.send(Message(text=random_line), thread_id=thread_id, thread_type=thread_type)
 
         try:
-            if author_id != self.uid: 
-                message = message_object.text.lower()
-                if "hi" in message: welcome_command()
-                elif "help" in message: help_command()
-                elif "info" in message: info_command()
-                elif "zoom" in message: zoom_command()
-                elif "troll" in message: troll_command()
-                elif "report" in message: report_command()
-
+            if author_id != self.uid:
+                message = message_object.text
+                if "hi" in message.lower(): welcome_command()
+                elif "help" in message.lower(): help_command()
+                elif "info" in message.lower(): info_command()
+                elif "zoom" in message.lower(): zoom_command()
+                elif "troll" in message.lower(): troll_command()
+                elif "report" in message.lower(): report_command()
+                elif "adel" in message.lower(): adel_info()
         except KeyboardInterrupt:
             print("[+] facebook bot terminated!")
             sys.exit(0)
